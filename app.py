a1c20cb49dc970d9cebb22a33dbb620b21a3d376
Lamido <uklamido@gmail.com>
2022-11-04 18:41:11 +0100

    final commit

--- app.py
+++ app.py
@@ -33,11 +33,11 @@ def api():
             elif "subtract" or "minus" in operation:
                 result = 0
                 for i in re.finditer(r'\d+', operation):
-                    result -= int(i.group())
+                    result = int(i.group()) - result
                 return jsonify({'slackUsername': 'Lamido', 'operation_type': 'subtraction', 'result': result})
 
         if "multiply" in operation:
-            result = 0
+            result = 1
             for i in re.finditer(r'\d+', operation):
                 result *= int(i.group())
             return jsonify({'slackUsername': 'Lamido', 'operation_type': 'multiplication', 'result': result})
